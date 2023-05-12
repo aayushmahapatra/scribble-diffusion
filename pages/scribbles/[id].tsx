715641d21055dba2577a6e8fcf8f8f6f0c497b2f
aayushmahapatra <mahapatraaayush@gmail.com>
2023-05-12 13:10:22 +0530

    Refactor getserversideprops

--- pages/scribbles/[id].tsx
+++ pages/scribbles/[id].tsx
@@ -1,27 +1,41 @@
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
 import Head from "next/head";
 import pkg from "package.json";
 import { Prediction } from "components/prediction";
 
-interface IScribble {
-  baseUrl: string;
-  prediction: any;
-}
+const Scribble: FC = () => {
+  const [baseUrl, setBaseUrl] = useState("");
+  const [prediction, setPrediction] = useState<any>();
 
-const Scribble: FC<IScribble> = ({ prediction, baseUrl }) => {
+  useEffect(() => {
+    async function fetchData() {
+      const protocol = (window as any).location.protocol || "http";
+      const predictionId = (window as any).location.pathname.split("/")[2];
+      const baseUrl = `${protocol}//${(window as any).location.host}`;
+      const response = await fetch(
+        `${baseUrl}/api/predictions/${predictionId}`
+      );
+      const prediction = await response.json();
+      setBaseUrl(baseUrl);
+      setPrediction(prediction);
+    }
+
+    fetchData();
+  }, []);
+
   return (
     <div>
       <Head>
         <title>
-          {prediction && `${prediction.input.prompt} - `}
+          {prediction && `${prediction?.input.prompt} - `}
           {pkg.appName}
         </title>
-        <meta name="description" content={prediction.input.prompt} />
+        <meta name="description" content={prediction?.input.prompt} />
         <meta property="og:title" content={pkg.appName} />
-        <meta property="og:description" content={prediction.input.prompt} />
+        <meta property="og:description" content={prediction?.input.prompt} />
         <meta
           property="og:image"
-          content={`${baseUrl}/api/og?id=${prediction.id}`}
+          content={`${baseUrl}/api/og?id=${prediction?.id}`}
         />
       </Head>
       <main className="container max-w-[1024px] mx-auto p-5">
@@ -32,14 +46,3 @@ const Scribble: FC<IScribble> = ({ prediction, baseUrl }) => {
 };
 
 export default Scribble;
-
-// Use getServerSideProps to force Next.js to render the page on the server,
-// so the OpenGraph meta tags will have the proper URL at render time.
-export async function getStaticProps({ req }) {
-  const protocol = req.headers.referer?.split("://")[0] || "http";
-  const predictionId = req.url.split("/")[2];
-  const baseUrl = `${protocol}://${req.headers.host}`;
-  const response = await fetch(`${baseUrl}/api/predictions/${predictionId}`);
-  const prediction = await response.json();
-  return { props: { baseUrl, prediction } };
-}
